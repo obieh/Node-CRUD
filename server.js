9b0c06c13c8f8079be22e4da14eed14e7aefa876
obieh <o_amadic@yahoo.com>
2025-05-05 14:29:28 +0100

    adding routes

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
